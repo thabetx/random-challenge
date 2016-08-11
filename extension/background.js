9f70d9fc771cb0ad8edfe54ab73499070561ef67
Mohamed Thabet <thabetx@gmail.com>
2016-08-11 23:22:33 +0200

    open a random codeforces problem

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,14 @@
+function Get(yourUrl){
+  var Httpreq = new XMLHttpRequest(); // a new request
+  Httpreq.open("GET",yourUrl,false);
+  Httpreq.send(null);
+  return Httpreq.responseText;          
+}
+
 chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.create({'url': "http://www.codeforces.com"});
+  var json_obj = JSON.parse(Get("http://codeforces.com/api/problemset.problems"));
+  rnd=Math.ceil(Math.random()*json_obj.result.problems.length);
+  contestId=json_obj.result.problems[rnd].contestId;
+  index=json_obj.result.problems[rnd].index
+  chrome.tabs.create({'url': "http://www.codeforces.com/problemset/problem/"+contestId+"/"+index});
 }); 
