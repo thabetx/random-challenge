97ab9fc94014617f380f0e242a821b101df5c673
Mohamed Thabet <thabetx@gmail.com>
2017-02-10 22:10:36 +0200

    code cleanup

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,13 @@
-function Get(yourUrl){
-  var Httpreq = new XMLHttpRequest(); // a new request
-  Httpreq.open("GET",yourUrl,false);
-  Httpreq.send(null);
-  return Httpreq.responseText;          
+function Get(URL){
+  var request = new XMLHttpRequest();
+  request.open("GET", URL, false);
+  request.send(null);
+  return request.responseText;
 }
 
 chrome.browserAction.onClicked.addListener(function() {
-  var json_obj = JSON.parse(Get("http://codeforces.com/api/problemset.problems"));
-  rnd=Math.ceil(Math.random()*json_obj.result.problems.length);
-  contestId=json_obj.result.problems[rnd].contestId;
-  index=json_obj.result.problems[rnd].index
-  chrome.tabs.create({'url': "http://www.codeforces.com/problemset/problem/"+contestId+"/"+index});
-}); 
+  var problemList = JSON.parse(Get("http://codeforces.com/api/problemset.problems"));
+  var rnd = Math.ceil(Math.random() * problemList.result.problems.length);
+  var problem = problemList.result.problems[rnd];
+  chrome.tabs.create({'url' : "http://www.codeforces.com/problemset/problem/"+problem.contestId+"/"+problem.index});
+});
